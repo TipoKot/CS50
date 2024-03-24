5285738bf40e1387e25f35bea1e586beec27ab0a
Aleksandr <35604321+TipoKot@users.noreply.github.com>
2024-03-24 09:47:16 +0200

    Added command-line argument support
    Takes a single command-line argument and if the program is executed without any command-line arguments or with more than one command-line argument, prints an error message and return from main a value of 1 immediately.

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,6 +1,15 @@
 #include <cs50.h>
+#include <stdio.h>
 
 int main(int argc, string argv[])
 {
+    if (argc == 2) // index 0 in argv is always the program name
+    {
 
+    }
+    else
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
 }
